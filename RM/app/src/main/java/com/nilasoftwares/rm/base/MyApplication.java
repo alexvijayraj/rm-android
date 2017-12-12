fb4cf95c88d1e52711bbda3ad2c7494a3a569969
Alex vijay raj Amalaraj <alexvijayraj@gmail.com>
2017-12-11 22:17:29 -0800

    more initial setup

--- RM/app/src/main/java/com/nilasoftwares/rm/base/MyApplication.java
+++ RM/app/src/main/java/com/nilasoftwares/rm/base/MyApplication.java
@@ -2,11 +2,18 @@ package com.nilasoftwares.rm.base;
 
 import android.app.Application;
 
+import com.nilasoftwares.rm.di.ActivityInjector;
+
+import javax.inject.Inject;
+
 /**
  * Created by alexvijayrajamalaraj on 12/11/17.
  */
 
 public class MyApplication extends Application {
+
+    @Inject
+    ActivityInjector activityInjector;
 
     private ApplicationComponent component;
 
@@ -17,5 +24,9 @@ public class MyApplication extends Application {
         component = DaggerApplicationComponent.builder()
                 .applicationModule(new ApplicationModule(this))
                 .build();
+    }
+
+    public ActivityInjector getActivityInjector() {
+        return activityInjector;
     }
 }
